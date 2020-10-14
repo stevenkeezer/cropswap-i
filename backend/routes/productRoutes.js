a4a2a5744af0e30c234a62f864b96342f9c32b1d
stevenkeezer <stevengkeezer@gmail.com>
2020-10-14 02:48:55 -0700

    first commit

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,26 @@
+import express from "express";
+import asyncHandler from "express-async-handler";
+const router = express.Router();
+import Product from "../models/productModel.js";
+import {
+  getProducts,
+  getTopProducts,
+  getProductById,
+  deleteProduct,
+  updateProduct,
+  createProduct,
+  createProductReview,
+} from "../controllers/productController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
+
+router.route("/").get(getProducts).post(protect, admin, createProduct);
+router.route("/:id/reviews").post(protect, createProductReview);
+router.get("/top", getTopProducts);
+
+router
+  .route("/:id")
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct);
+
+export default router;
